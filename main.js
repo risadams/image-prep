dfe6874c92b6c9d06a96cff23f180c703fc8d934
Ris Adams <chris@tuesdaybegins.com>
2022-06-03 09:34:42 -0400

    shell icon

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const { app, BrowserWindow } = require('electron');
 
-function createMainWindow(){
-  const mainWindow = new BrowserWindow({
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     title: 'Image-Prep',
+    icon: `${__dirname}/assets/icons/ris1.png`,
   });
+
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
 }
 
-app.on('ready',createMainWindow);
+app.on('ready', createMainWindow);
